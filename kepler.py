Fix orbital elements calculation

kepler.py
@@ -1,8 +1,6 @@
 import math
 import numpy as np
 
-# "stolen" from OrbitSim3D, modified for use with Numpy
-# https://github.com/arda-guler/orbitSim3D/blob/master/orbit.py
 def state2kepler(r_vec, v_vec):
         r = np.dot(r_vec, r_vec)
         v = np.dot(v_vec, v_vec)
@@ -15,43 +13,37 @@ def state2kepler(r_vec, v_vec):
         # inc: inclination
         # lon_asc: longitude/right ascension of ascending node
 
-        v_r = np.dot(v_vec, r_vec/r)
-        v_t = (v**2 - v_r**2)**0.5
+        h = np.cross(r_vec, v_vec)
+        e = np.cross(v_vec, h) / mu - r_vec / np.linalg.norm(r_vec)
 
-        sma = 1/(2/r - v**2/mu)
+        n = np.cross(np.transpose(np.array([0, 0, 1])), h)
 
-        h_vec = np.cross(r_vec, v_vec)
-        h = np.dot(h_vec, h_vec)
-
-        inc = math.acos(h_vec[2] / h)
-
-        K = np.array([0, 0, 1])
-        N_vec = np.cross(K, h_vec)
-        N = np.dot(N_vec, N_vec)
-
-        if not N:
-            if (r_vec[1] > 0 and v_vec[0] > 0) or (r_vec[1] < 0 and v_vec[0] < 0):
-                lon_asc = math.pi * 1.5
-            else:
-                lon_asc = math.pi * 0.5
-
+        if np.dot(r_vec, v_vec) > 0:
+                nu = np.arccos(np.dot(e, r_vec)/(np.linalg.norm(e) * np.linalg.norm(r_vec)))
         else:
-            lon_asc = math.acos(N_vec[0] / N)
-            if N_vec[1] < 0:
-                lon_asc = 2 * math.pi - lon_asc
+                nu = 2 * np.pi - np.arccos(np.dot(e, r_vec)/(np.linalg.norm(e) * np.linalg.norm(r_vec)))
 
-        e_vec = np.cross(v_vec, h_vec) / mu - r_vec / r
-        e = np.dot(e_vec, e_vec)
+        inc = np.arccos(h[2] / np.linalg.norm(h))
+        ecc = np.linalg.norm(e)
+        E = 2 * np.arctan((np.tan(nu / 2)) / np.sqrt((1 + ecc) / (1 - ecc)))
 
-        if (N * e):
-            arg_peri = math.acos(np.dot(N_vec, e_vec) / (N * e))
-            if e_vec[2] < 0:
-                arg_peri = 2 * math.pi - arg_peri
+        if n[1] >= 0:
+                lon_asc = np.arccos(n[0] / np.linalg.norm(n))
         else:
-            arg_peri = math.pi * 0.5
+                lon_asc = 2 * np.pi - np.arccos(n[0] / np.linalg.norm(n))
 
+        if e[2] >= 0:
+                arg_peri = np.arccos(np.dot(n, e) / (np.linalg.norm(n) * np.linalg.norm(e)))
+        else:
+                arg_peri = 2 * np.pi - np.arccos(np.dot(n, e) / (np.linalg.norm(n) * np.linalg.norm(e)))
+
+        M = E - ecc * np.sin(E)
+
+        sma = 1 / (2/np.linalg.norm(r_vec) - np.linalg.norm(v_vec)**2 / mu)
+        
         return {"a": sma,
-                "e": e,
+                "e": ecc,
                 "i": np.rad2deg(inc),
                 "arg_peri": np.rad2deg(arg_peri),
                 "lon_asc": np.rad2deg(lon_asc)}
+